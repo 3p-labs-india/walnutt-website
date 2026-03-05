ba2c4693736442e1fa1747b74b87e34003ae6462
prasenjit2311 <prasenjit2311@gmail.com>
2026-03-05 17:32:11 +0530

    Add PostHog init to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@
   import { createRoot } from "react-dom/client";
   import App from "./app/App.tsx";
   import "./styles/index.css";
+  import { initPostHog } from "./lib/analytics";
+
+  initPostHog();
 
   createRoot(document.getElementById("root")!).render(<App />);
-  
